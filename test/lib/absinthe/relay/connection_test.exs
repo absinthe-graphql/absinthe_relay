c9d7c939b1daf1623f962806905f6237d83df969
Bruce Williams <brwcodes@gmail.com>
2016-04-29 10:50:50 -0700

    Move connection test to separate file

---
+++ test/lib/absinthe/relay/connection_test.exs
@@ -0,0 +1,204 @@
+defmodule Absinthe.Relay.ConnectionTest do
+  use ExSpec, async: true
+
+  defmodule CustomConnectionAndEdgeFieldsSchema do
+    use Absinthe.Schema
+    use Absinthe.Relay.Schema
+
+    @people %{
+      "jack" => %{id: "jack", name: "Jack", age: 35, pets: ["1", "2"]},
+      "jill" => %{id: "jill", name: "Jill", age: 31, pets: ["3"]}
+    }
+
+    @pets %{
+      "1" => %{id: "1", name: "Svenja"},
+      "2" => %{id: "2", name: "Jock"},
+      "3" => %{id: "3", name: "Sherlock"}
+    }
+
+    node object :pet do
+      field :name, :string
+      field :age, :string
+    end
+
+    connection node_type: :pet do
+      field :twice_edges_count, :integer do
+        resolve fn
+          _, %{source: conn} ->
+            {:ok, length(conn.edges) * 2}
+        end
+      end
+      edge do
+        field :node_name_backwards, :string do
+          resolve fn
+            _, %{source: edge} ->
+              {:ok, edge.node.name |> String.reverse}
+          end
+        end
+      end
+    end
+
+    node object :person do
+      field :name, :string
+      field :age, :string
+
+      @desc "The pets for a person"
+      connection field :pets, node_type: :pet do
+        resolve fn
+          resolve_args, %{source: person} ->
+            {
+              :ok,
+              Absinthe.Relay.Connection.from_list(
+                Enum.map(person.pets, &Map.get(@pets, &1)),
+                resolve_args
+              )
+            }
+        end
+      end
+
+    end
+
+    query do
+
+      node field do
+        resolve fn
+          %{type: :person, id: id}, _ ->
+            {:ok, Map.get(@people, id)}
+        end
+      end
+
+    end
+
+    node interface do
+      resolve_type fn
+        %{age: _}, _ ->
+          :person
+        _, _ ->
+          nil
+      end
+    end
+
+  end
+
+  describe "Defining custom connection and edge fields" do
+    it "allows querying those additional fields" do
+      result = """
+        query FirstPetName($personId: ID!) {
+          node(id: $personId) {
+            ... on Person {
+              pets(first: 1) {
+                twiceEdgesCount
+                edges {
+                  nodeNameBackwards
+                  node {
+                    name
+                  }
+                }
+              }
+            }
+          }
+        }
+      """ |> Absinthe.run(CustomConnectionAndEdgeFieldsSchema, variables: %{"personId" => @jack_global_id})
+      assert {:ok, %{data: %{"node" => %{"pets" => %{"twiceEdgesCount" => 2, "edges" => [%{"nodeNameBackwards" => "ajnevS", "node" => %{"name" => "Svenja"}}]}}}}} = result
+    end
+
+    defmodule CustomConnectionAndEdgeFieldsSchema do
+      use Absinthe.Schema
+      use Absinthe.Relay.Schema
+
+      @people %{"jack" => %{id: "jack", name: "Jack", age: 35, pets: ["1", "2"]},
+                "jill" => %{id: "jill", name: "Jill", age: 31, pets: ["3"]}}
+      @pets %{"1" => %{id: "1", name: "Svenja"},
+              "2" => %{id: "2", name: "Jock"},
+              "3" => %{id: "3", name: "Sherlock"}}
+
+      node object :pet do
+        field :name, :string
+        field :age, :string
+      end
+
+      connection node_type: :pet do
+        field :twice_edges_count, :integer do
+          resolve fn
+            _, %{source: conn} ->
+              {:ok, length(conn.edges) * 2}
+          end
+        end
+        edge do
+          field :node_name_backwards, :string do
+            resolve fn
+              _, %{source: edge} ->
+                {:ok, edge.node.name |> String.reverse}
+            end
+          end
+        end
+      end
+
+      node object :person do
+        field :name, :string
+        field :age, :string
+
+        @desc "The pets for a person"
+        connection field :pets, node_type: :pet do
+          resolve fn
+            resolve_args, %{source: person} ->
+              {
+                :ok,
+                Absinthe.Relay.Connection.from_list(
+                  Enum.map(person.pets, &Map.get(@pets, &1)),
+                  resolve_args
+                )
+              }
+          end
+        end
+
+      end
+
+      query do
+
+        node field do
+          resolve fn
+            %{type: :person, id: id}, _ ->
+              {:ok, Map.get(@people, id)}
+          end
+        end
+
+      end
+
+      node interface do
+        resolve_type fn
+          %{age: _}, _ ->
+            :person
+          _, _ ->
+            nil
+        end
+      end
+
+    end
+
+    describe "Defining custom connection and edge fields" do
+      it "allows querying those additional fields" do
+        result = """
+          query FirstPetName($personId: ID!) {
+             node(id: $personId) {
+               ... on Person {
+                 pets(first: 1) {
+                   twiceEdgesCount
+                   edges {
+                     nodeNameBackwards
+                     node {
+                       name
+                     }
+                   }
+                 }
+               }
+             }
+           }
+        """ |> Absinthe.run(CustomConnectionAndEdgeFieldsSchema, variables: %{"personId" => @jack_global_id})
+        assert {:ok, %{data: %{"node" => %{"pets" => %{"twiceEdgesCount" => 2, "edges" => [%{"nodeNameBackwards" => "ajnevS", "node" => %{"name" => "Svenja"}}]}}}}} = result
+      end
+
+    end
+  end
+
+end
