66d5afd968fb068ce7e71e0338bd83c0f2d07bf8
Sebastian Kosch <sebastian@aldusleaf.org>
2016-06-05 16:15:44 +0200

    Update absinthe dependency to 1.1.6

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule AbsintheRelay.Mixfile do
 
   defp deps do
     [
-      {:absinthe, "~> 1.1.0"},
+      {:absinthe, "~> 1.1.6"},
       {:ecto, "~> 1.0 or ~> 2.0", optional: true},
       {:poison, ">= 0.0.0", only: [:dev, :test]},
       {:ex_doc, "~> 0.11.0", only: :dev},
