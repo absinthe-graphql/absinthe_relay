694d21c46ea107fe58a584e65120e6c4b9bab4d6
Bruce Williams <brwcodes@gmail.com>
2017-03-27 23:26:14 -0700

    [v1.3] Flexible parsing_node_ids (#50)
    * Better errors, multiple types
    
    - Return error on parse failure rather than a thrown `ArgumentError`.
    - Support checking against multiple types.
    
    *From https://elixir-lang.slack.com/archives/absinthe-graphql/p1480927370000208*
    > I've had a need for checking the node ID type against many rather than just one (in the case of interfaces) so I've played around with `parsing_node_ids/2` to accommodate the requirement.  It's a non-breaking change and fixes an issue where an error should be returned rather than a thrown error.
    > The only concern you may have is that it returns `%{ type, id }` or `id` depending on `expected_type(s)`, mainly to prevent this being a breaking change, and also that it makes sense.
    
    * Swap results
    
    Silly me
    
    * Expand tests related to parsing_node_ids with a test proof that the current implementation is incapable of handling a situation where the first node id in a set of multiple node id arguments fails to parse, resulting in a Map.get exception.
    
    * Handle multiple errors, node types
    
    * Update to latest absinthe v1.3 beta

--- lib/absinthe/relay/node/helpers.ex
+++ lib/absinthe/relay/node/helpers.ex
@@ -1,43 +1,80 @@
 defmodule Absinthe.Relay.Node.Helpers do
+  @moduledoc """
+  Useful schema helper functions for node IDs.
+  """
 
   alias Absinthe.Relay.Node
 
   @doc """
   Wrap a resolver to parse node (global) ID arguments before it is executed.
 
+  For each argument:
+
+  - If a single node type is provided, the node ID in the argument map will
+    be replaced by the ID specific to your application.
+  - If multiple node types are provided (as a list), the node ID in the
+    argument map will be replaced by a map with the node ID specific to your
+    application as `:id` and the parsed node type as `:type`.
+7
   ## Examples
 
-  Parse a node (global) ID argument `:item_id` (which should be an ID for the
-  `:item` type)
+  Parse a node (global) ID argument `:item_id` as an `:item` type. This replaces
+  the node ID in the argument map (key `:item_id`) with your
+  application-specific ID. For example, `"123"`.
 
   ```
   resolve parsing_node_ids(&my_field_resolver/2, item_id: :item)
   ```
+
+  Parse a node (global) ID argument `:interface_id` into one of multiple node
+  types. This replaces the node ID in the argument map (key `:interface_id`)
+  with map of the parsed node type and your application-specific ID. For
+  example, `%{type: :thing, id: "123"}`.
+
+  ```
+  resolve parsing_node_ids(&my_field_resolver/2, interface_id: [:item, :thing])
+  ```
   """
-  def parsing_node_ids(resolver, id_keys) do
+  def parsing_node_ids(resolver, rules) do
     fn args, info ->
-      args = Enum.reduce(id_keys, args, fn {key, type}, args ->
+      Enum.reduce(rules, {%{}, []}, fn {key, expected_type}, {node_id_args, errors} ->
         with {:ok, global_id} <- Map.fetch(args, key),
-             {:ok, %{id: id, type: ^type}} <- Node.from_global_id(global_id, info.schema) do
-          {:success, Map.put(args, key, id)}
+             {:ok, node_id} <- Node.from_global_id(global_id, info.schema),
+             {:ok, node_id} <- check_node_id(node_id, expected_type, key) do
+          {Map.put(node_id_args, key, node_id), errors}
+        else
+          {:error, msg} ->
+            {node_id_args, [msg | errors]}
         end
-        |> case do
-             {:ok, %{type: bad_type}} ->
-               # The user provided an ID for a different type of field,
-               # notify them in a normal GraphQL error response
-               {:error, "Invalid node type for argument `#{key}`; should be #{type}, was #{bad_type}"}
-             {:error, msg} ->
-               # A more serious error, eg, a missing type, notify
-               # the schema designer with an exception
-               raise ArgumentError, msg
-             {:success, args} ->
-               args
-             _ ->
-               args
-           end
       end)
-      resolver.(args, info)
+      |> case do
+        {node_id_args, []} ->
+          resolver.(Map.merge(args, node_id_args), info)
+        {_, errors} ->
+          {:error, Enum.reverse(errors)}
+      end
     end
   end
 
+  defp check_node_id(node_id = %{ type: type }, expected_types, key) when is_list(expected_types) do
+    if type in expected_types do
+      {:ok, node_id}
+    else
+      {
+        :error,
+        ~s<In argument "#{key}": Expected node type in #{inspect(expected_types)}, found #{inspect(type)}.>
+      }
+    end
+  end
+
+  defp check_node_id(%{type: type, id: id}, expected_type, key) do
+    if type == expected_type do
+      {:ok, id}
+    else
+      {
+        :error,
+        ~s<In argument "#{key}": Expected node type #{inspect(expected_type)}, found #{inspect(type)}.>
+      }
+    end
+  end
 end
