746da15df798c7035c2d7ad373bb70c2c97d3aa4
Bruce Williams <brwcodes@gmail.com>
2017-08-21 20:03:36 -0700

    Restructure for Classic/Modern split
    This does some basic restructuring to the mutation
    notation modules and cleans up the macro logic. It also:
    
    - Adds the deprecation warning for defaulting behavior
    - Add some additional examples, docs

--- lib/absinthe/relay/mutation/modern_notation.ex
+++
@@ -1,9 +0,0 @@
-defmodule Absinthe.Relay.Mutation.ModernNotation do
-  use Absinthe.Relay.Mutation.Base
-
-  defp client_mutation_id_field do
-    quote do
-      field :client_mutation_id, type: :string
-    end
-  end
-end
