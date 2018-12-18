ad2502ac61730096b94b9aa377101eebbeeaed51
Blake Gentry <bgentry@users.noreply.github.com>
2018-12-18 15:44:48 -0800

    support ecto >= 2 (#125) (#129)
    * support ecto >= 2
    
    * limit the range of ecto version

--- mix.exs
+++ mix.exs
@@ -46,7 +46,7 @@ defmodule AbsintheRelay.Mixfile do
   defp deps do
     [
       {:absinthe, "~> 1.4.0"},
-      {:ecto, "~> 2.0", optional: true},
+      {:ecto, "~> 2.0 or ~> 3.0", optional: true},
       {:poison, ">= 0.0.0", only: [:dev, :test]},
       {:ex_doc, "~> 0.16", only: :dev}
     ]
