122bad45ff166985076d11df484bb0dc40d8b742
Ben Wilson <benwilson512@gmail.com>
2022-03-22 08:01:06 -0400

    version bump and updates

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule AbsintheRelay.Mixfile do
   use Mix.Project
 
   @source_url "https://github.com/absinthe-graphql/absinthe_relay"
-  @version "1.5.1"
+  @version "1.5.2"
 
   def project do
     [
@@ -56,7 +56,6 @@ defmodule AbsintheRelay.Mixfile do
     [
       {:absinthe, "~> 1.5.0 or ~> 1.6.0 or ~> 1.7.0"},
       {:ecto, "~> 2.0 or ~> 3.0", optional: true},
-      {:poison, ">= 0.0.0", only: [:dev, :test]},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
