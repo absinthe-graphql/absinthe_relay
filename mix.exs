e249eace322a8b9b2592c31c563a36996793849d
Kian-Meng Ang <kianmeng@cpan.org>
2023-11-05 10:54:34 +0800

    Support Erlang OTP/26 and Elixir 15
    List of chanages:
    - add Erlang OTP/26 and Elixir to GitHub CI
    - bump outdated deps, see https://hex.pm/l/HgEiR
    - bump GitHub actions
    - set Elixir 1.11 as minimum requirement

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule AbsintheRelay.Mixfile do
     [
       app: :absinthe_relay,
       version: @version,
-      elixir: "~> 1.10",
+      elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
